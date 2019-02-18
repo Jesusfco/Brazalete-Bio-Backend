7b05161a3ef2d22e990650f3f627df0d83eed4db
Jesús Fco Cortés <jfcr@live.com>
2019-02-18 14:04:06 -0600

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,36 +9,22 @@ use Auth;
 use App\User;
 
 class LoginController extends Controller
-{
-    
-    // use AuthenticatesUsers;
-
-  
-    //  */
-    // protected $redirectTo = '/home';
-
-    // /**
-    //  * Create a new controller instance.
-    //  *
-    //  * @return void
-    //  */
-    // public function __construct()
-    // {
-    //     $this->middleware('guest')->except('logout');
-    // }
+{       
 
     public function loginApi(Request $re) {
 
         if(Auth::attempt(['email' => $re->email, 'password' => $re->password]) ) {
+
             $token = new Token();
             $token->user_id = Auth::id();
             $token->generateKey();
             $token->save();
 
-            return response()->json(['token', $token->token]);
+            return response()->json(['token', $token->token, 'user', Auth::user()]);
         } 
 
         return response()->json(['msj' => 'Credentials Incorrect'], 403);
 
     }
+
 }
